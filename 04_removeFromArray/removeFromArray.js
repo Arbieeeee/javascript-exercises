ac4e30f3aa8ebf23e6ecbec5b918455df2aedef9
Arbieeeee <k.delosreyes1126@gmail.com>
2024-12-08 22:01:28 +0800

    fix: Added func to removeFromArray based on testcases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removeElement) {
+  let filteredArr = [];
+  let isInRemove;
+
+  for (let i = 0; i < arr.length; i++) {
+    isInRemove = false;
+    for (let j = 0; j < removeElement.length; j++) {
+      if (arr[i] === removeElement[j]) {
+        isInRemove = true;
+        break;
+      }
+    }
+    if (!isInRemove) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
 };
 
 // Do not edit below this line
