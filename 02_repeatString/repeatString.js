65f4ed7040d0168abfa44c204f4acd86e1acbe96
Arbieeeee <k.delosreyes1126@gmail.com>
2024-12-08 21:02:28 +0800

    fix: Added a function to repeat a string based on test case

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, word) {
+  let newInput = "";
 
+  if (word < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= word; i++) {
+    newInput += string;
+  }
+  return newInput;
 };
 
 // Do not edit below this line
