bb3f9fe910acf0729c9804e5025c5896e5089086
Arbieeeee <k.delosreyes1126@gmail.com>
2024-12-09 11:28:01 +0800

    fix: Added fun for summing all nums

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  let smaller = 0;
+  let larger = 0;
+  let result = 0;
+  if (
+    first >= 0 &&
+    Number.isInteger(first) &&
+    last >= 0 &&
+    Number.isInteger(last)
+  ) {
+    if (first < last) {
+      smaller = first;
+      larger = last;
+    } else {
+      smaller = last;
+      larger = first;
+    }
 
+    let i = smaller;
+    do {
+      result += i;
+      i++;
+    } while (i <= larger);
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
